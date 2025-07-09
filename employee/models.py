63de6a24eea7e85f61c4353bb2b7009b568cb545
Preeti Gurung <gurungpreeti999@gmail.com>
2025-07-09 16:12:09 +0545

    fetched data from the database

--- employee/models.py
+++ employee/models.py
@@ -9,3 +9,6 @@ class Employee(models.Model):
     email = models.EmailField(max_length=100,unique=True)
     phone_number = models.CharField(max_length=12, blank=True)
     image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.first_name
--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from employee.models import Employee
 
 def home(request):
     return render(request,'home.html')
 
 def contact(request):
-    return HttpResponse("Hello from contact")
+    employee = Employee.objects.all()
+    context={
+        'employee': employee
+    }
+    print("data from view", employee)
+    return render(request,'contact.html',context)
